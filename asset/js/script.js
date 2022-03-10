2ebde32eaef287a9f3d9e11f7768d2646610a4b3
Michael Freitas <michael_rfreitas@hotmail.com>
2022-03-10 22:08:58 +0000

    script: create a script.js file and functions

---
+++ asset/js/script.js
@@ -0,0 +1,31 @@
+function playerName () {
+
+}
+
+function startQuiz() {
+
+}
+
+function randomQuestion() {
+    
+}
+
+function checkAnswer() {
+
+}
+
+function addScore() {
+
+}
+
+function reduceScore() {
+
+}
+
+function timeAnswer() {
+
+}
+
+function displayResult() {
+
+}
