da0068b62ba8ce1aba8e779b314dd6f0d0bcedca
Michael Freitas <michael_rfreitas@hotmail.com>
2022-03-11 11:52:16 +0000

    script: add interaction with quiz page

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+			if (this.getAttribute("data-type") === "submit") {
+                let name = document.getElementById("name").value;
+                startQuiz(name);
+			} else {
+				let gameType = this.getAttribute("data-type");
+			}
+		});
+	}
 })
+
+function startQuiz(name) {
+    sessionStorage.setItem("name", name);
+}
+
+randomQuestion();
 
 async function getQuestions() {
     try {
@@ -11,26 +28,34 @@ async function getQuestions() {
     }
 }
 
-async function readQuestions() {
+async function randomQuestion() {
     let index = Math.floor(Math.random() * 20);
     let questions = await getQuestions();
-        document.getElementById("question").textContent = questions[index].Questions;
-
-}
-
-readQuestions();
-
-
-function playerName () {
-
-}
-
-function startQuiz() {
-
-}
-
-function randomQuestion() {
-    
+    document.getElementById("name").textContent = sessionStorage.getItem("name");
+    document.getElementById("question").textContent = questions[index].Questions;
+    let letter = Math.floor(Math.random() * 3) + 1;
+    if (letter === 1) {
+        document.getElementById("img_a").src = "./asset/media/" + questions[index].B + ".jpg";
+        document.getElementById("img_b").src = "./asset/media/" + questions[index].C + ".jpg";
+        document.getElementById("img_c").src = "./asset/media/" + questions[index].A + ".jpg";
+        document.getElementById("text_a").textContent = questions[index].B;
+        document.getElementById("text_b").textContent = questions[index].C;
+        document.getElementById("text_c").textContent = questions[index].A;
+    } else if (letter === 2) {
+        document.getElementById("img_a").src = "./asset/media/" + questions[index].C + ".jpg";
+        document.getElementById("img_b").src = "./asset/media/" + questions[index].A + ".jpg";
+        document.getElementById("img_c").src = "./asset/media/" + questions[index].B + ".jpg";
+        document.getElementById("text_a").textContent = questions[index].C;
+        document.getElementById("text_b").textContent = questions[index].A;
+        document.getElementById("text_c").textContent = questions[index].B;
+    } else {
+        document.getElementById("img_a").src = "./asset/media/" + questions[index].A + ".jpg";
+        document.getElementById("img_b").src = "./asset/media/" + questions[index].B + ".jpg";
+        document.getElementById("img_c").src = "./asset/media/" + questions[index].C + ".jpg";
+        document.getElementById("text_a").textContent = questions[index].A;
+        document.getElementById("text_b").textContent = questions[index].B;
+        document.getElementById("text_c").textContent = questions[index].C;
+    }
 }
 
 function checkAnswer() {
