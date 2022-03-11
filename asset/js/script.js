37f0a8c09e0e6489e11663b0aba394cc4b1022c3
Michael Freitas <michael_rfreitas@hotmail.com>
2022-03-11 01:42:05 +0000

    script: add fetch json file in js and get question

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+})
+
+async function getQuestions() {
+    try {
+        let res = await fetch("./asset/js/questions.json");
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function readQuestions() {
+    let index = Math.floor(Math.random() * 20);
+    let questions = await getQuestions();
+        document.getElementById("question").textContent = questions[index].Questions;
+
+}
+
+readQuestions();
+
+
 function playerName () {
 
 }
